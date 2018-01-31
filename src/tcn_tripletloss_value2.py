d2db1f99c998ce7fc256d71a00c2d4cbed2f248c
ryosism <sn0wbreeze2000@yahoo.co.jp>
2018-01-31 09:48:01 +0000

    [add] DP solver

--- src/tcn_tripletloss_value2.py
+++ src/tcn_tripletloss_value2.py
@@ -12,7 +12,6 @@ import keras.backend.tensorflow_backend as KTF
 
 import numpy as np
 from glob import glob
-from os import path
 import os
 import sys
 import time
@@ -174,24 +173,87 @@ def random_batch_generator(train_list, num_batches = 32):
                 anc = []
                 neg = []
 
-def make_tensor(order_num):
+def make_tensor(all_confidences, order_num, alpha):
 
+    # all_confidences[#手順画像][#frame]
 
+    table = np.zeros([order_num,order_num])
+    np.set_printoptions(precision=3, suppress=True) #指数表示の禁止
+
+    for i in range(0,order_num):
+        table[i][i] = 0.1
+
+    for i in range(0,order_num-1):
+        table[i][i+1] = 0.2
+
+    for i in range(0,order_num-1):
+        table[i+1][i] = 0.4
+
+    for c in range(2, order_num):
+        for b in range(0, order_num-c):
+            table[b][b+c] = 0.4*(c-1)
+
+    for c in range(1, order_num):
+        for b in range(0, order_num-c):
+            table[b+c][b] = 0.4*c
+
+    # ここまでで重みテーブルは完成
+    print(table)
+    table = table * alpha
+    print(table)
+
+    # ノード作成、と同時にノードの値(confidence)も設定
+    G = nx.DiGraph()
+    G.add_node(0, value = 0)
+
+    frames = len(all_confidences[0])
+    for i in range(frames * order_num):
+        G.add_node(i+1)
+
+        # try:
+        G.nodes[i+1]['value'] = all_confidences[int(i % order_num)][int(i/order_num)]
+        #     break
+        # except ValueError:
+        #     print("[ValueError] int(i % order_num) = {}, int(i/order_num) = {}".format(int(i % order_num), int(i/order_num)))
+    print("{} nodes created.".format(i))
+
+    G.add_node(frames * order_num+1, value = 0)
+
+    # スタートから最初のキーへのエッジ
+    for i in range(1,order_num+2):
+        G.add_edge(0, i, weight = 1)
+
+    # 全エッジに対して重み設定
+    for t in range(frames-1):
+        for i in range(order_num*(t)+1, order_num*(t+1)+1):
+            for j in range(order_num*(t+1)+1, order_num*(t+2)+1):
+                if i < 100:
+                    print("i, j, {}, {}, weight = {}(table[{}][{}])+ {}".format(i, j, table[(i % order_num)-1][(j % order_num)-1], (i % order_num)-1, (j % order_num)-1, G.nodes[j]['value']))
+                elif i > 96650:
+                    print("i, j, {}, {}, weight = {}(table[{}][{}])+ {}".format(i, j, table[(i % order_num)-1][(j % order_num)-1], (i % order_num)-1, (j % order_num)-1, G.nodes[j]['value']))
+
+                G.add_edge(i, j, weight = (table[i % order_num][j % order_num] + G.nodes[j]['value']))
+
+    # 最後のノードからゴールへのエッジ
+    for i in range((frames-1) * order_num, frames * order_num+1):
+        G.add_edge(i, frames * order_num+1, weight = 1)
+
+    return G
 
 def print_path(prev, cost):
     for i in range(len(prev)):
         print("%d, prev = %d, cost = %d" %  (i, prev[i], cost[i]))
 
 def get_path(start, goal, prev):
-    path = []
+    route = []
     now = goal
-    path.append(now)
+    route.append(now)
     while True:
-        path.append(prev[now])
+        route.append(prev[now])
         if prev[now] == start: break
         now = prev[now]
-    path.reverse()
-    return path
+    route.reverse()
+    return route
 
 def search(glaph, start, goal):
     MAX_VAL = 0x10000000
@@ -218,7 +280,6 @@ def search(glaph, start, goal):
                 next = i
         if next == -1: break
         now = next
-    print_path(prev, cost)
     return [get_path(start, goal, prev), cost[goal]]
 
 
@@ -255,8 +316,8 @@ def test():
     model = build_predict(base_model, input_shape=input_shape)
     model.summary()
 
-    dlist_v1 = glob(path.join(sys.argv[1], '*'))
-    dlist_v2 = glob(path.join(sys.argv[2], '*'))
+    dlist_v1 = glob(os.path.join(sys.argv[1], '*'))
+    dlist_v2 = glob(os.path.join(sys.argv[2], '*'))
 
     dlist_v1.sort()
     dlist_v2.sort()
@@ -274,10 +335,12 @@ def test():
 
     query = 0
     correct = 0
+    all_distances = []
+    all_confidences = []
 
     for dirs in zip(dlist_v1, dlist_v2):
-        flist_v1 = glob(path.join(dirs[0], '*.png'))
-        flist_v2 = glob(path.join(dirs[1], '*.png'))
+        flist_v1 = glob(os.path.join(dirs[0], '*.png'))
+        flist_v2 = glob(os.path.join(dirs[1], '*.png'))
         flist_v1.sort()
         flist_v2.sort()
         # print (len(flist_v1), len(flist_v2))
@@ -320,6 +383,7 @@ def test():
             nn = 0
 
             confidences = []
+            distances = []
             for j in range(min_index, len(q_feats)):
                 dist = np.linalg.norm( r_feat[0] - q_feats[j][0])
 
@@ -330,7 +394,9 @@ def test():
                     min_dist = dist
                     nn = j
 
+                distances.append(dist)
                 confidences.append(1.0 - dist)
+
 
                 # top5
                 # rank_dist, rank_index = top5(rank_dist, rank_index, dist, j)
@@ -339,12 +405,16 @@ def test():
                 # rank_dist, rank_index = top500(rank_dist, rank_index, dist, j)
 
             confidence_graph(confidences, i)
+            all_distances.append(distances)
+            all_confidences.append(confidences)
+
+            na = np.array(all_confidences, dtype=np.float)
+            logger.log(30, np.shape)
 
             # top1
             logger.log(30, '{} {} {}'.format(i, nn, min_dist))
             key_index.append(nn)
             logger.log(30, 'key_index = {}'.format(key_index))
-
 
             # top5
             # logger.log(30, '{} rank_dist = {} rank_index = {}'.format(i, rank_dist, rank_index))
@@ -356,6 +426,19 @@ def test():
 
             if i == nn:
                 correct += 1
+
+        # path, cost = search(make_tensor(all_confidences, len(flist_v1), 1), 0, len(flist_v1)*len(flist_v2))
+        path = nx.dijkstra_path(make_tensor(all_distances, len(flist_v1), 1.5), 0, len(flist_v1)*len(flist_v2))
+
+        order_path = []
+        now = -1
+        for pa in path:
+            pa = pa % len(flist_v1)
+            if now != pa:
+                now = pa
+                order_path.append(pa)
+
+        print("order_path = ",order_path)
 
     if correct == 0:
         logger.log(30, "{} files, {} corrects, accuracy = zero divided".format(query, correct))
